2b98cf16f4170d007811fc16bcabfd84c3e9ce4b
Abrar Kassim <46764578+abrark04@users.noreply.github.com>
2025-02-15 16:38:54 -0500

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import login from "./login"
+
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>Artemis</h1>
-    </>
-  );
+  // return (
+  //   <>
+  //     <h1>Artemis</h1>
+  //   </>
+  // );
+
+  
+    return <login/>;
+  
+  
 }
+
+
+
+
 
 export default App;
