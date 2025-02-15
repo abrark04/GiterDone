f4693f7bdee853859998d07dbe01ca820b6687d2
Kerushani <kerushanisivaneswaran@gmail.com>
2025-02-15 16:55:07 -0500

    Add to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import login from "./login"
-
+import login from "./login";
+import ImgUploader from "./components/ImgUploader.jsx"
 
 function App() {
   const [count, setCount] = useState(0);
 
-  // return (
-  //   <>
-  //     <h1>Artemis</h1>
-  //   </>
-  // );
-
-  
-    return <login/>;
-  
-  
+  return (
+    <>
+      <h1>Artemis</h1>
+      <login />
+    </>
+  );
 }
-
-
-
-
 
 export default App;
